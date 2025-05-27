529cdd931a19c4ce41a411c5cfafd64210db309e
Tao Xie <oliver.xietao@gmail.com>
2025-05-27 17:05:29 -0400

    Initial MVP version

--- src/__init__.py
+++ src/__init__.py
@@ -2,5 +2,5 @@ from .input_handler import InputHandler
 from .calculator import InvestmentCalculator
 from .visualizer import DataVisualizer
 from .report_generator import ReportGenerator
-
+ 
 __all__ = ['InputHandler', 'InvestmentCalculator', 'DataVisualizer', 'ReportGenerator'] 
